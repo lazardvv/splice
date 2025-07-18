fec77866fa36c5db9eedeef025c297afd9bb04a6
Lazar Drazic <lazar.drazic@htecgroup.com>
2025-07-18 11:18:05 +0200

    first added splitter functionalitiities

--- src/components/Connection.jsx
+++ src/components/Connection.jsx
@@ -3,14 +3,18 @@ import Adress from './Adress'
 import { useState } from 'react'
 import Cassete from './Cassete'
 import AdressesList from './AdressesList'
-import Splitters from './Splitters'
+
 import SpliterTest from './SpliterTest'
+import { adresses, initalPorts } from '../constants'
 
 const Connection = () => {
 
   const [index, setIndex] = useState(0)
   const [splicedAddresses, setSplicedAddresses] = useState([]);
+  const [port, setPort] = useState(initalPorts)
   
+
+
 
 
   function handleNextAdress() {
@@ -21,19 +25,17 @@ const Connection = () => {
   }
 
   return (
-    <div>{/* Connection component to manage addresses and cassettes 
+    <div>
       <div className='flex justify-between items-center mb-4'>
         <Adress index={index} />
-        <Splitters />
+
+        <SpliterTest need={adresses[index].users} port={port} setPort={setPort} />
+
       </div>
 
       <AdressesList index={index} setIndex={setIndex} splicedAddresses={splicedAddresses} />
       <button className='bg-black p-2 m-2 rounded-full text-white hover:bg-gray-200' onClick={handleNextAdress}>Next adress</button>
-      <Cassete index={index} setSplicedAddresses={setSplicedAddresses} />*/ }
-
-
-      <SpliterTest need={3} />
-
+      <Cassete index={index} setSplicedAddresses={setSplicedAddresses} />
     </div>
   )
 }
